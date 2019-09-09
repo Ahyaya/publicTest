e0841bc691b10cb3841a67dc195bc27072eb2cc0
Chan <40376613+Ahyaya@users.noreply.github.com>
2019-09-09 10:29:55 +0800

    update ubuntu-app pre-setting
    remove some apps with domestically unavailable sources

--- ubuntu_InstallApps.sh
+++ ubuntu_InstallApps.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install vim screen zip gcc g++ gfortran git google-chrome-stable octave fcitx qtiplot chrome-gnome-shell gnome-shell-extensions
-sudo add-apt-repository ppa:webupd8team/atom  
-sudo apt-get update
-sudo apt-get install atom texlive-full texmaker libreoffice libstdc++6:i386 libgcc1:i386 libcurl4-gnutls-dev:i386
+sudo apt install vim screen zip gcc g++ gfortran git libstdc++6:i386 libgcc1:i386 libcurl4-gnutls-dev:i386 default-jre default-jdk
+sudo apt install fcitx octave qtiplot texlive-full texmaker libreoffice
+
+#sudo apt install google-chrome-stable chrome-gnome-shell gnome-shell-extensions
+#sudo add-apt-repository ppa:webupd8team/atom  
+#sudo apt-get update
+
 cd ~/Documents
 mkdir deepin_wine
 cd deepin_wine
@@ -14,6 +17,6 @@ sudo bash install.sh
 cd ~/Documents/deepin_wine
 wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin.com.qq.im/deepin.com.qq.im_8.9.19983deepin23_i386.deb
 sudo dpkg -i deepin.com.qq.im_8.9.19983deepin23_i386.deb
-sudo add-apt-repository multiverse
-sudo apt update
+#sudo add-apt-repository multiverse
+#sudo apt update
 sudo apt install steam vlc
