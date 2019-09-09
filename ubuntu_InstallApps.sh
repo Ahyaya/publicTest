3cf4834dc070a5986effe0591114ed28778806fd
Chan <40376613+Ahyaya@users.noreply.github.com>
2019-09-09 13:26:35 +0800

    domestic version

--- ubuntu_InstallApps.sh
+++ ubuntu_InstallApps.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install vim screen zip gcc g++ gfortran git libstdc++6:i386 libgcc1:i386 libcurl4-gnutls-dev:i386 default-jre default-jdk
-sudo apt install fcitx octave qtiplot texlive-full texmaker libreoffice
-
-#sudo apt install google-chrome-stable chrome-gnome-shell gnome-shell-extensions
-#sudo add-apt-repository ppa:webupd8team/atom  
-#sudo apt-get update
+sudo apt install vim screen zip gcc g++ gfortran git libstdc++6:i386 libgcc1:i386 libcurl4-gnutls-dev:i386 default-jre
+sudo apt install curl fcitx octave qtiplot texlive-full texmaker libreoffice gnome-tweak-tool steam
+sudo apt install chrome-gnome-shell gnome-shell-extensions
 
 cd ~/Documents
 mkdir deepin_wine
@@ -17,6 +14,3 @@ sudo bash install.sh
 cd ~/Documents/deepin_wine
 wget http://mirrors.aliyun.com/deepin/pool/non-free/d/deepin.com.qq.im/deepin.com.qq.im_8.9.19983deepin23_i386.deb
 sudo dpkg -i deepin.com.qq.im_8.9.19983deepin23_i386.deb
-#sudo add-apt-repository multiverse
-#sudo apt update
-sudo apt install steam vlc
