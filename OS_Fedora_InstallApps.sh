e41bd465f53179fcf21c9e5f4e7ea5453f4c373e
Chan <40376613+Ahyaya@users.noreply.github.com>
2019-10-14 13:22:20 +0800

    operating system setup

--- OS_Fedora_InstallApps.sh
+++ OS_Fedora_InstallApps.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo dnf install vim screen gcc g++ gfortran curl octave texmaker
-
 #use rpmfusion as source
 sudo dnf install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install --nogpgcheck http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -14,5 +12,11 @@ sudo dnf upgrade
 #install 3rd-part media decoder
 sudo dnf install gstreamer gstreamer-ffmpeg gstreamer-plugins-bad gstreamer-plugins-bad-free gstreamer-plugins-bad-nonfree gstreamer-plugins-base gstreamer-plugins-good gstreamer-plugins-ugly gstreamer-plugin-crystalhd gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free gstreamer1-plugins-good gstreamer1-libav ffmpeg
 
-sudo dnf install steam qtox
+#install generic compilers and tools
+sudo dnf install vim screen gcc curl octave octave-devel python-pip python3-pip
 
+#install LaTeX combos with CTeX toolkits
+sudo dnf install texlive texlive-ctex texlive-cjk texlive-cjkpunct texlive-fandol texlive-xecjk texmaker
+
+#some entertainment software
+sudo dnf install steam qtox
