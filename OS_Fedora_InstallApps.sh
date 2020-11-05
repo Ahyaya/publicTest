9b4157266cf47a4ed3f7168a476420def3a41992
Chan <40376613+Ahyaya@users.noreply.github.com>
2020-11-05 20:32:58 +0800

    Update OS_Fedora_InstallApps.sh

--- OS_Fedora_InstallApps.sh
+++ OS_Fedora_InstallApps.sh
@@ -10,13 +10,16 @@ sudo dnf makecache
 sudo dnf upgrade
 
 #install 3rd-part media decoder
-sudo dnf install gstreamer gstreamer-ffmpeg gstreamer-plugins-bad gstreamer-plugins-bad-free gstreamer-plugins-bad-nonfree gstreamer-plugins-base gstreamer-plugins-good gstreamer-plugins-ugly gstreamer-plugin-crystalhd gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free gstreamer1-plugins-good gstreamer1-libav ffmpeg
+sudo dnf install gstreamer gstreamer-ffmpeg gstreamer-plugins-bad gstreamer-plugins-bad-free gstreamer-plugins-bad-nonfree gstreamer-plugins-base gstreamer-plugins-good gstreamer-plugins-ugly gstreamer-plugin-crystalhd gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free gstreamer1-plugins-good gstreamer1-libav
 
 #install generic compilers and tools
-sudo dnf install vim screen gcc curl octave octave-devel python-pip python3-pip
+sudo dnf install vim screen gcc curl octave octave-devel python3-pip
+
+#install generic compilers and tools
+sudo dnf install ffmpeg libpng15 kolourpaint
 
 #install LaTeX combos with CTeX toolkits
-sudo dnf install texlive texlive-ctex texlive-cjk texlive-cjkpunct texlive-fandol texlive-xecjk texmaker
+sudo dnf install texlive texlive-ctex texlive-cjk texlive-cjkpunct texlive-fandol texlive-xecjk texlive-preprint texlive-makecell texlive-multirow texlive-moreverb texlive-wrapfig texlive-revtex4 texlive-appendix texmaker
 
 #some entertainment software
-sudo dnf install steam qtox
+#sudo dnf install steam qtox
